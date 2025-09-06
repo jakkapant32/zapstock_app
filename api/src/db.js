e25fc44b0db6352188f738d2f4fe887aa723f2c9
jakkapant32 <jakkapant32@gmail.com>
2025-09-06 22:57:09 +0700

    Update db.js

--- api/src/db.js
+++ api/src/db.js
@@ -3,7 +3,12 @@ const config = require('../config');
 
 const pool = new Pool({
   connectionString: config.database.url,
-  ssl: config.database.ssl
+  ssl: config.database.ssl,
+  max: 20, // เพิ่ม connection pool
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  keepAlive: true,
+  keepAliveInitialDelayMillis: 0
 });
 
 module.exports = {
