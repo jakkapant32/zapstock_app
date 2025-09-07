347543018bcc1d5e8c792e05436d11873bb5d7ef
jakkapant32 <jakkapant32@gmail.com>
2025-09-07 15:23:53 +0700

    Update server.js

--- api/src/server.js
+++ api/src/server.js
@@ -29,7 +29,10 @@ const allowedOrigins = config.cors?.allowedOrigins || [
   'http://10.214.162.160:3000',
   'http://10.214.162.160:3001',
   'http://169.254.41.48:3000',
-  'http://169.254.13.29:3000'
+  'http://169.254.13.29:3000',
+  'https://zapstock-backend.onrender.com',
+  'https://*.onrender.com',
+  '*'
 ];
 
 app.use(cors({
